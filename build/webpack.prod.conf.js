9dc5c736597d1d0e7b320bd7e5238ddd3806d48f
lichengzhi <lichengzhi@cnicg.cn>
2018-05-15 11:17:56 +0800

    set env mode

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -19,6 +19,7 @@ const env =
     : require('../config/prod.env')
 
 const webpackConfig = merge(baseWebpackConfig, {
+  mode: 'production',
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
